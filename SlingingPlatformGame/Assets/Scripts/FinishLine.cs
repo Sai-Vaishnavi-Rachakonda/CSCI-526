7f8f4b0b855940ca7def57cce15b2e5d3f047170
Sai-Vaishnavi-Rachakonda <111935118+Sai-Vaishnavi-Rachakonda@users.noreply.github.com>
2023-02-20 15:01:47 -0800

    git commit -m "level design"

--- SlingingPlatformGame/Assets/Scripts/FinishLine.cs
+++ SlingingPlatformGame/Assets/Scripts/FinishLine.cs
@@ -14,6 +14,7 @@ public class FinishLine : MonoBehaviour
     private AnalyticsObj dbObj = new AnalyticsObj();
     private static DateTime dt = DateTime.Now;
     public string nextScene;
+    public int score;
     void Start()
     {
         
@@ -34,7 +35,7 @@ public class FinishLine : MonoBehaviour
             dbObj.setTimeLine(timeLine);
             dbObj.setOutcome(1);
             postToDatabase(dbObj);
-             if (player_script.ScoreNum >= score)
+           if (player_script.ScoreNum >= score)
             {
                 SceneManager.LoadScene(nextScene); //send the player to the next level.
             }
