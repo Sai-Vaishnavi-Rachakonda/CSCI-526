67d906da1fbff0114fc86017e3f5fca7e981b126
ShreeyaTahasildar <shreeya.t97@gmail.com>
2023-02-26 10:11:15 -0800

    Added key collection and door color change

--- SlingingPlatformGame/Assets/Scripts/player_script.cs
+++ SlingingPlatformGame/Assets/Scripts/player_script.cs
@@ -6,18 +6,19 @@ using UnityEngine.SceneManagement;
 
 public class player_script : MonoBehaviour
 {
-    //public Text MyscoreText;
-    // public static float ScoreNum;
+    public Text MyscoreText;
+    public static float ScoreNum;
+    public static float maxScore;
     
-    public static float MaxHealth;
-    public Slider _slide;
-    public static float currentHealth;
-    private int level;
+    //public static float MaxHealth;
+    // public Slider _slide;
+    // public static float currentHealth;
+    
 
     void Start()
     {
-        //ScoreNum = 0;
-        // MyscoreText.text = "Energy level : " + ScoreNum + "/10";
+        ScoreNum = 0;
+        //MyscoreText.text = "Keys Collected : " + ScoreNum + "/10";
         Scene currentScene = SceneManager.GetActiveScene ();
  
          // Retrieve the name of t$$anonymous$$s scene.
@@ -25,37 +26,44 @@ public class player_script : MonoBehaviour
  
          if (sceneName == "Level 0") 
          {
-            MaxHealth = 10;
-            level = 0;
+            maxScore = 1;
+            MyscoreText.text = "Keys Collected : " + ScoreNum + "/" + maxScore;
+            
          }
          else if(sceneName == "Level 1")
          {
-            MaxHealth = 9;
-            level = 1;
+            maxScore = 3;
+            MyscoreText.text = "Keys Collected : " + ScoreNum + "/" + maxScore;
+            
          }
-        currentHealth = 0;
-        _slide.maxValue = MaxHealth;
-        _slide.value = 0;
+        // currentHealth = 0;
+        // _slide.maxValue = MaxHealth;
+        // _slide.value = 0;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("powerup"))
-        {
-            //ScoreNum += 0.5f;
-            //Destroy(collision.gameObject);
-            // MyscoreText.text = "Energy level : " + ScoreNum + "/10";
-            if (level == 0) 
-            {
-                currentHealth = currentHealth + 10;
-            }
-            else if(level == 1)
-            {
-                currentHealth = currentHealth + 3;
-            }
+        // if(collision.CompareTag("powerup"))
+        // {
+        //     //ScoreNum += 0.5f;
+        //     //Destroy(collision.gameObject);
+        //     // MyscoreText.text = "Energy level : " + ScoreNum + "/10";
+        //     if (level == 0) 
+        //     {
+        //         currentHealth = currentHealth + 10;
+        //     }
+        //     else if(level == 1)
+        //     {
+        //         currentHealth = currentHealth + 3;
+        //     }
             
-            _slide.value = currentHealth;
+        //     _slide.value = currentHealth;
+        //     Destroy(collision.gameObject);
+        // }
+        if(collision.CompareTag("Key")){
+            ScoreNum += 1;
             Destroy(collision.gameObject);
+            MyscoreText.text = "Keys Collected : " + ScoreNum + "/" + maxScore;
         }
     }
 }
