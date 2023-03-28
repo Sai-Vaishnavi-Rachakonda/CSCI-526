78244e1f0c2956e9d8b703ed27c86d9cc97b56e6
PratikKodolikar <pratikgk98@gmail.com>
2023-03-28 02:54:46 -0700

    Level 6 update
    Added new elements in level 6 - enemy, spikes, and level design

--- SlingingPlatformGame/Assets/Scripts/Enemy.cs
+++ SlingingPlatformGame/Assets/Scripts/Enemy.cs
@@ -8,29 +8,31 @@ public class Enemy : MonoBehaviour
     public float leftLimit = 1.5f; // the left limit of the enemy's movement
     public float rightLimit = 4.5f; // the right limit of the enemy's movement
     public float initial_position_x;
+    public float range = 6.0f;
 
-    // void start()
-    // {
-    //     initial_position_x = transform.position.x;
-    //     Debug.Log("initial position - "+ initial_position_x);
-    // }
+    void start()
+    {
+        initial_position_x = transform.position.x;
+        Debug.Log("initial position - "+ initial_position_x);
+    }
     // Update is called once per frame
     void Update()
     {
         // move the enemy horizontally
-        Debug.Log("initial position - "+ initial_position_x);
+        Debug.Log("Update section - initial position - "+ initial_position_x);
+        Debug.Log("X value:"+ transform.position.x);
         transform.Translate(Vector2.right * speed * Time.deltaTime);
         // float initial_position_x = transform.position.x;
         // check if the enemy has gone past the right limit
-        if (transform.position.x > initial_position_x+2.0f)
+        if (transform.position.x > initial_position_x + range)
         {
             // move the enemy to the left limit
-            transform.position = new Vector2(initial_position_x+2.0f, transform.position.y);
+            transform.position = new Vector2(initial_position_x + 2.0f, transform.position.y);
             transform.Translate(Vector2.left * speed * Time.deltaTime);
         }
-        if (transform.position.x < initial_position_x-2.0f)
+        if (transform.position.x < initial_position_x - range)
         {
-            transform.position = new Vector2(initial_position_x-2.0f, transform.position.y);
+            transform.position = new Vector2(initial_position_x - 2.0f, transform.position.y);
             transform.Translate(Vector2.right * speed * Time.deltaTime);
         }
     }
