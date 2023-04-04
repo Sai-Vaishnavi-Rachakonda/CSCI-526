f6d95b845f4146b49fcd86799af7e20d84e22ddf
chiragpandey15 <chiragpandey15@gmail.com>
2023-04-03 18:14:56 -0700

    Update PlayerMovement.cs

--- SlingingPlatformGame/Assets/Scripts/PlayerMovement.cs
+++ SlingingPlatformGame/Assets/Scripts/PlayerMovement.cs
@@ -117,14 +117,16 @@ public class PlayerMovement : MonoBehaviour
             var diff = Camera.transform.position.y - transform.position.y;
 
             if(SceneManager.GetActiveScene().name=="Level 5"){
-                if(Camera.transform.position.x>=45){
+                if((transform.position.x>=45 && transform.position.x<=65) || transform.position.x>76){
                     Camera.transform.position = new Vector3(Camera.transform.position.x, Camera.transform.position.y - Time.deltaTime*diff*6, Camera.transform.position.z);
                 }
-                    
                 else if(Camera.transform.position.y>=1.1)
                     Camera.transform.position = new Vector3(Camera.transform.position.x, Camera.transform.position.y - Time.deltaTime*diff, Camera.transform.position.z);
             }else if(SceneManager.GetActiveScene().name=="Level 7"){    
-                Camera.transform.position = new Vector3(Camera.transform.position.x, Camera.transform.position.y - Time.deltaTime*diff*6, Camera.transform.position.z);
+                if(transform.position.x>=45)
+                    Camera.transform.position = new Vector3(Camera.transform.position.x, Camera.transform.position.y - Time.deltaTime*diff*6, Camera.transform.position.z);
+                else if(Camera.transform.position.y>=1.1)
+                    Camera.transform.position = new Vector3(Camera.transform.position.x, Camera.transform.position.y - Time.deltaTime*diff, Camera.transform.position.z);
                 
             }else if(Camera.transform.position.y>=1.1) // Initial position of camera aprox 0
                 Camera.transform.position = new Vector3(Camera.transform.position.x, Camera.transform.position.y - Time.deltaTime*diff, Camera.transform.position.z);
